08993c5f8a8f6ac2bc2687f190dcb23be9728766
sgonzalo75 <sgonzalo75@gmail.com>
2015-01-05 21:53:41 +0100

    --

--- projects/OpenMote-CC2538/03oos_openwsn_csl/Debug/OpenMote-CC2538/eui64.d
+++
@@ -1,5 +0,0 @@
-OpenMote-CC2538/eui64.o: \
- /home/sgonzalos/TFM/openwsn-trunk/openwsn-fw/bsp/boards/OpenMote-CC2538/eui64.c \
- /home/sgonzalos/TFM/openwsn-trunk/openwsn-fw/bsp/boards/eui64.h
-
-/home/sgonzalos/TFM/openwsn-trunk/openwsn-fw/bsp/boards/eui64.h:
--- projects/OpenMote-CC2538/03oos_openwsn_csl/Debug/OpenMote-CC2538/source/debug.d
+++
@@ -1,8 +0,0 @@
-OpenMote-CC2538/source/debug.o: \
- /home/sgonzalos/TFM/openwsn-trunk/openwsn-fw/bsp/boards/OpenMote-CC2538/source/debug.c \
- /home/sgonzalos/TFM/openwsn-trunk/openwsn-fw/bsp/boards/OpenMote-CC2538/source/debug.h \
- /home/sgonzalos/TFM/openwsn-trunk/openwsn-fw/bsp/boards/OpenMote-CC2538/headers/hw_types.h
-
-/home/sgonzalos/TFM/openwsn-trunk/openwsn-fw/bsp/boards/OpenMote-CC2538/source/debug.h:
-
-/home/sgonzalos/TFM/openwsn-trunk/openwsn-fw/bsp/boards/OpenMote-CC2538/headers/hw_types.h:
